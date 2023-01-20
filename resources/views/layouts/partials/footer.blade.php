e2011f58057bab8479aa19b83d375fab45f99d3f
Azhar Ibn Mostafiz <theazharul@gmail.com>
2023-01-20 14:45:51 +0600

    footer commented

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.3
-    </div>
-    <strong>Copyright &copy; 2023 <a href="https://techituki.com">TechiTuki</a>.</strong> All rights
-    reserved.
-  </footer>
+  <!-- <div class="float-right d-none d-sm-block">
+       <b>Version</b> 3.0.3
+       </div>
+       <strong>Copyright &copy; 2023 <a href="https://techituki.com">TechiTuki</a>.</strong> All rights
+       reserved. -->
+</footer>
