8c404e8f61f6b810aeafcd8490971f645c4dc4d1
Azhar Ibn Mostafiz <theazharul@gmail.com>
2023-01-20 14:43:53 +0600

    invoice template added

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,4 +1,98 @@
 @extends('layouts.admin')
 @section('content')
-<h1>Order details </h1>
+<div class="container">
+<div class="row">
+    				<!-- BEGIN INVOICE -->
+					<div class="col-xs-12">
+						<div class="grid invoice">
+							<div class="grid-body">
+								<div class="invoice-title">
+									<div class="row">
+										<div class="col-xs-12">
+											<img src="http://vergo-kertas.herokuapp.com/assets/img/logo.png" alt="" height="35">
+										</div>
+									</div>
+									<br>
+									<div class="row">
+										<div class="col-sm-6">
+											<h2>invoice<br><br>
+											<span class="small">order #{{$order->id}}</span></h2>
+										</div>
+                                        <div class="col-sm-6 text-right">
+<button class="" onclick="window.print()">Print invoice</button>
+                                        </div>
+									</div>
+								</div>
+								<hr>
+								<!-- <div class="row">
+									 <div class="col-xs-6">
+									 <address>
+									 <strong>Billed To:</strong><br>
+									 Twitter, Inc.<br>
+									 795 Folsom Ave, Suite 600<br>
+									 San Francisco, CA 94107<br>
+									 <abbr title="Phone">P:</abbr> (123) 456-7890
+									 </address>
+									 </div>
+								     </div> -->
+								<div class="row">
+									<div class="col-xs-6 text-right">
+										<address>
+											<strong>Order Date: </strong>{{$order->created_at}}
+
+										</address>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<h3>ORDER SUMMARY</h3>
+										<table class="table table-striped">
+											<thead>
+												<tr class="line">
+													<td><strong>#</strong></td>
+													<td class="text-center"><strong>Product Name</strong></td>
+													<td class="text-center"><strong>Quantity</strong></td>
+													<td class="text-right"><strong>Price</strong></td>
+													<td class="text-right"><strong>Subtotal</strong></td>
+												</tr>
+											</thead>
+											<tbody>
+
+   @foreach($order->items as $index => $item)
+
+												<tr>
+													<td>{{$index+1}}</td>
+													<td><strong>{{$item->product->name}}</strong></td>
+													<td class="text-center">{{$item->quantity}}</td>
+													<td class="text-center">{{$item->price}}৳</td>
+													<td class="text-right">{{$item->quantity * $item->price}}৳</td>
+												</tr>
+   @endforeach
+												<tr>
+													<td colspan="3">
+													</td><td class="text-right"><strong>Total</strong></td>
+													<td class="text-right"><strong>{{$order->total()}}৳</strong></td>
+												</tr>
+                                                @if($order->discount != 0)
+												<tr>
+													<td colspan="3"></td>
+													<td class="text-right"><strong>Discount</strong></td>
+													<td class="text-right"><strong>{{$order->discount}}৳</strong></td>
+												</tr>
+                                                @endif
+												<tr>
+													<td colspan="3"></td>
+													<td class="text-right"><strong>Grand Total</strong></td>
+													<td class="text-right"><strong>{{$order->total() - $order->discount}}৳</strong></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- END INVOICE -->
+				</div>
+</div>
 @endsection
